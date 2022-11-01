6326bd955663c09088c7496f085804846289deaf
adidamd <adidamd@gmail.com>
2022-11-01 10:22:47 +0530

    add a python_file

---
+++ cre.c
@@ -0,0 +1,4 @@
+name = input('Enter the name : ')
+age = int(input('Enter you age : '))
+color = input('Enter lucky colour: ')
+printf('Your name is '+name+' and your age is '+age+' and your colour is '+color)
